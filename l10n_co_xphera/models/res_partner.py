02726efa743966ae4755fd37e989e1197b1b2920
Bradember <bybeltranc@unal.edu.co>
2021-08-31 11:23:19 -0500

    anglo setting

--- l10n_co_xphera/models/res_partner.py
+++ l10n_co_xphera/models/res_partner.py
@@ -37,17 +37,17 @@ class AccountChartTemplate(models.Model):
 
         config_settings = self.env['res.config.settings'].with_context(company=company)
 
-        config_settings.use_anglo_saxon = True
-
         config_settings.group_analytic_accounting = True
         config_settings.module_account_budget = True
-        config_settings.module_product_margin = True       
+        config_settings.module_product_margin = True
+        config_settings.use_anglo_saxon = True
         config_settings.group_analytic_tags = True
 
         config_settings_all = self.env['res.config.settings'].search([('company_id', '=', company.id)], limit=1)
         config_settings_all.group_analytic_accounting = True
         config_settings_all.module_account_budget = True
         config_settings_all.module_product_margin = True
+        config_settings_all.use_anglo_saxon = True
         config_settings_all.group_analytic_tags = True
 
         config_settings.account_tax_periodicity_reminder_day = 3
